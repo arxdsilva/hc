d2e13883b59009d9330679c4a38aa55d97f94f50
Arthur Silva <arxdsilva@gmail.com>
2017-12-17 11:14:44 -0200

    refactor: add dSettingsFromInput as method
        add setGrid

--- coord.go
+++ coord.go
@@ -3,33 +3,52 @@ package main
 import (
 	"errors"
 	"strconv"
+	"strings"
 )
 
 type dSettings struct {
-	gridSize    []int64
+	grid        []int64
 	coords      []int64
 	commands    string
 	camPosition string
 }
 
-func dSettingsFromInput(s string) (d dSettings, err error) {
+func (d *dSettings) dSettingsFromInput(s string) (err error) {
 	if len(s) == 0 {
-		return d, errors.New("input cannot be empty")
+		return errors.New("input cannot be empty")
 	}
 	coords := s[0:4]
 	x := coords[:2]
 	xInt, err := strconv.ParseInt(x, 10, 64)
 	if err != nil {
-		return d, err
+		return
 	}
 	d.coords = append(d.coords, xInt)
 	y := s[2:4]
 	yInt, err := strconv.ParseInt(y, 10, 64)
 	if err != nil {
-		return d, err
+		return
 	}
 	d.coords = append(d.coords, yInt)
 	d.camPosition = string(s[4])
 	d.commands = s[5:]
-	return d, err
+	return
+}
+
+func (d *dSettings) setGrid(s string) (err error) {
+	if len(s) == 0 {
+		return errors.New("input cannot be empty")
+	}
+	gridSplitted := strings.Split(s, "x")
+	gridX, err := strconv.ParseInt(gridSplitted[0], 10, 64)
+	if err != nil {
+		return
+	}
+	gridY, err := strconv.ParseInt(gridSplitted[1], 10, 64)
+	if err != nil {
+		return
+	}
+	d.grid = append(d.grid, gridX)
+	d.grid = append(d.grid, gridY)
+	return
 }
