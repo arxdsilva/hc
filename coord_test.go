52d528ac254b84725c43d8e0dd866039c9762400
Arthur Silva <arxdsilva@gmail.com>
2017-12-17 10:59:23 -0200

    coord_test: add test cases

--- coord_test.go
+++ coord_test.go
@@ -3,7 +3,11 @@ package main
 import check "gopkg.in/check.v1"
 
 func (s *S) TestStripInput(c *check.C) {
-	str, err := stripInput("0315ODEDEFFDFFD")
-	c.Assert(str, check.NotNil)
+	_, err := dSettingsFromInput("")
+	c.Assert(err, check.NotNil)
+	d, err := dSettingsFromInput("0315ODEDEFFDFFD")
+	c.Assert(len(d.coords), check.Equals, 2)
+	c.Assert(d.camPosition, check.NotNil)
+	c.Assert(d.commands, check.NotNil)
 	c.Assert(err, check.IsNil)
 }
