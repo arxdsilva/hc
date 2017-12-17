619e8cf945ae728398579526b00e0cc62f825a93
Arthur Silva <arxdsilva@gmail.com>
2017-12-17 10:59:51 -0200

    add dSettingsFromInput

--- coord.go
+++ coord.go
@@ -1,5 +1,35 @@
 package main
 
-func stripInput(s string) (string, err error) {
-	return nil, nil
+import (
+	"errors"
+	"strconv"
+)
+
+type dSettings struct {
+	gridSize    []int64
+	coords      []int64
+	commands    string
+	camPosition string
+}
+
+func dSettingsFromInput(s string) (d dSettings, err error) {
+	if len(s) == 0 {
+		return d, errors.New("input cannot be empty")
+	}
+	coords := s[0:4]
+	x := coords[:2]
+	xInt, err := strconv.ParseInt(x, 10, 64)
+	if err != nil {
+		return d, err
+	}
+	d.coords = append(d.coords, xInt)
+	y := s[2:4]
+	yInt, err := strconv.ParseInt(y, 10, 64)
+	if err != nil {
+		return d, err
+	}
+	d.coords = append(d.coords, yInt)
+	d.camPosition = string(s[4])
+	d.commands = s[5:]
+	return d, err
 }
