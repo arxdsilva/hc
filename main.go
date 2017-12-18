67e09de74064a2805182acac38497cfdd136e553
Arthur Silva <arxdsilva@gmail.com>
2017-12-17 23:09:14 -0200

    main: add initial settings

--- main.go
+++ main.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"os"
+)
+
+var mappedGrid map[int64][]int64
+
+func main() {
+	args := os.Args
+	if (len(args) == 0) || (len(args) > 1) {
+		log.Fatal("bot needs 1 grid")
+	}
+	err := validateGrid(args[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+	d := dSettings{}
+	err = d.setGrid(args[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
