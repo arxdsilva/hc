709460926601b2ad8b40cf4b73bb50b0390ddad9
arthur silva <arxdsilva@gmail.com>
2018-04-14 01:39:32 -0300

    acc/reader: move generateAccountsFromRaw and create readCSV

---
+++ acc/reader.go
@@ -0,0 +1,11 @@
+package main
+
+import "encoding/csv"
+
+func generateAccountsFromRaw(file string) (accs map[int64]int64, err error) {
+	return
+}
+
+func readCSV(r csv.Reader) (rec [][]string, err error) {
+	return
+}
