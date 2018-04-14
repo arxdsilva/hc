a1ba144e4d6d51fd5c975f15d24481a91c26592b
arthur silva <arxdsilva@gmail.com>
2018-04-14 02:15:33 -0300

    acc/reader: add mapAccs
    also adds initial logic to generateAccountsFromRaw

--- acc/reader.go
+++ acc/reader.go
@@ -1,11 +1,42 @@
 package main
 
-import "encoding/csv"
+import (
+	"bufio"
+	"encoding/csv"
+	"os"
+	"strconv"
+)
 
 func generateAccountsFromRaw(file string) (accs map[int64]int64, err error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return
+	}
+	r := csv.NewReader(bufio.NewReader(f))
+	_, err = readCSV(r)
+	if err != nil {
+		return
+	}
+
 	return
 }
 
 func readCSV(r *csv.Reader) (rec [][]string, err error) {
 	return r.ReadAll()
+}
+
+func mapAccs(accs [][]string) (map[int]int, error) {
+	accsMap := map[int]int{}
+	for _, acc := range accs {
+		key, err := strconv.Atoi(acc[0])
+		if err != nil {
+			return nil, err
+		}
+		value, err := strconv.Atoi(acc[1])
+		if err != nil {
+			return nil, err
+		}
+		accsMap[key] = value
+	}
+	return accsMap, nil
 }
