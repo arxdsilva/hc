74f16f6bc8e74b0c62f957b3aea3b5536ba6914d
arthur silva <arxdsilva@gmail.com>
2018-04-14 10:37:22 -0300

    acc/main: initial client setup

--- acc/main.go
+++ acc/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
+func main() {
+	args := os.Args
+	if len(args) < 3 {
+		log.Fatal("Tool must have 2 args")
+	}
+	fmt.Printf("reading file: %v\n", args[1])
+	accs, err := generateAccountsFromRaw(args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("reading file: %v\n", args[2])
+	_, err = calcBalancesAfterTransactions(args[2], accs)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
